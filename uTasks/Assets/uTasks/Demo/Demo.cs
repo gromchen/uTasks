b9a1f3efa9c599d1b9bd007c707d6b0d7a482802
Yuriy Gromchenko <gromchen@outlook.com>
2015-06-29 12:44:07 +0300

    Used thread pool only, instead of co-routines, optimized unity thread dispatcher

--- uTasks/Assets/uTasks/Demo/Demo.cs
+++ uTasks/Assets/uTasks/Demo/Demo.cs
@@ -1,4 +1,5 @@
 ﻿using JetBrains.Annotations;
+using uTasks.Dispatchers;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,13 +7,21 @@ namespace uTasks.Demo
 {
     public class Demo : MonoBehaviour
     {
-        private CancellationTokenSource _tokenSource;
+        #region Editor
 
+        [SerializeField] private Text _resultText;
+        [SerializeField] private InputField _nInputField;
+        [SerializeField] private Button _calculateButton;
+        [SerializeField] private Button _cancelButton;
+        [SerializeField] private UnityThreadDispatcher _dispatcher;
+
+        #endregion
+
+        private CancellationTokenSource _tokenSource;
+        
         [UsedImplicitly]
         private void Awake()
         {
-            MainThread.Current = new UnityMainThread();
-
             _resultText.text = "Doing nothing";
             _calculateButton.onClick.AddListener(StartCalculation);
             _cancelButton.onClick.AddListener(CancelCalculation);
@@ -30,8 +39,8 @@ namespace uTasks.Demo
 
             _tokenSource = new CancellationTokenSource();
             _resultText.text = "Computing";
-            TaskFactory.StartNew(() => FindPrimeNumber(n, _tokenSource.Token))
-                .CompleteWithAction(task =>
+            Task.Run(() => FindPrimeNumber(n, _tokenSource.Token))
+                .ThenInvoke(_dispatcher, task =>
                 {
                     if (task.IsCanceled)
                     {
@@ -82,14 +91,5 @@ namespace uTasks.Demo
 
             return (--a);
         }
-
-        #region Editor
-
-        [SerializeField] private Text _resultText;
-        [SerializeField] private InputField _nInputField;
-        [SerializeField] private Button _calculateButton;
-        [SerializeField] private Button _cancelButton;
-
-        #endregion
     }
 }
